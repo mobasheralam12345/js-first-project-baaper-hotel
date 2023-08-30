6ec74f0fcb371a949bf99bcbaedf718c3471733b
MobasherAlam <190139.cse@student.just.edu.bd>
2023-08-31 00:52:20 +0600

    project done

--- js/withdraw.js
+++ js/withdraw.js
@@ -6,20 +6,32 @@ document.getElementById('withdraw-btn').addEventListener('click',function(){
     const withdrawInputField = document.getElementById('withdraw-input-field');
     const withdrawInputFieldString = withdrawInputField.value;
     const withdrawInput = parseFloat(withdrawInputFieldString);
-   
+    
+    withdrawInputField.value = '';
+
+    if(isNaN(withdrawInput)){
+        alert('Please Provide a valid number');
+        return;
+    }
+
     const withdrawH2Total = document.getElementById('withdraw-h2-total');
     const withdrawH2TotalString = withdrawH2Total.innerText;
     const withdrawH2 = parseFloat(withdrawH2TotalString);
+   
+    const totalWithdrawBalance = document.getElementById('total-balance');
+    const totalWithdrawBalanceString = totalWithdrawBalance.innerText;
+    const totalWithdraw = parseFloat(totalWithdrawBalanceString);
+   
+
+    if(withdrawInput > totalWithdraw ){
+        alert('ato tk nai');
+        return;
+    }
 
     const newWithdraw = withdrawInput + withdrawH2;
     withdrawH2Total.innerText = newWithdraw;
 
-    const totalWithdrawBalance = document.getElementById('total-balance');
-    const totalWithdrawBalanceString = totalWithdrawBalance.innerText;
-    const totalWithdraw = parseFloat(totalWithdrawBalanceString);
-
     const finalWithdraw = totalWithdraw - withdrawInput;
     totalWithdrawBalance.innerText = finalWithdraw;
 
-    withdrawInputField.value = '';
 })
