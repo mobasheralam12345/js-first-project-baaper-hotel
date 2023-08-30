e5e8eae4e080deb447e11b495cc895a2b52b2e00
MobasherAlam <190139.cse@student.just.edu.bd>
2023-08-31 01:09:43 +0600

    final

--- js/deposit.js
+++ js/deposit.js
@@ -6,19 +6,31 @@
         const newDepositAmountString = depositInputField.value;
         const newDepositAmount = parseFloat(newDepositAmountString);
 
+        depositInputField.value = '';
+
+        if(isNaN(newDepositAmount)){
+            alert('Please Provide me a valid number');
+            return;
+        }
+
         const depositTotalElement = document.getElementById('deposit-h2-total');
         const previousDepositTotalString = depositTotalElement.innerText;
         const previousDeposit = parseFloat(previousDepositTotalString);
 
-        const currentDepositTotal = previousDeposit + newDepositAmount;
-        depositTotalElement.innerText = currentDepositTotal;
-
+        
         const balanceTotalElement = document.getElementById('total-balance');
         const previousBalanceTotalString = balanceTotalElement.innerText;
         const previousBalanceTotal = parseFloat(previousBalanceTotalString);
 
+        if(newDepositAmount > previousBalanceTotal){
+            alert('Please Provide me Enough amount');
+            return;
+         }
+
+        const currentDepositTotal = previousDeposit + newDepositAmount;
+        depositTotalElement.innerText = currentDepositTotal;
+
         const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
         balanceTotalElement.innerText = currentBalanceTotal;
         
-        depositInputField.value = '';
     })
